1a7730bf7b9f67f702887a6c4cb6c8efdbf68f08
box3bdp <101050018+box3bdp@users.noreply.github.com>
2022-09-02 20:01:31 +0800

    Add files via upload

---
+++ sign-in/index.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    var oUser = document.getElementsByName("username");
+    var oPwd = document.getElementsByName("password");
+    var oBtn = document.getElementsByClassName("btn");
+    var oText = document.getElementByClassName("text");
+
+    var Members=['build_一只建筑师吖'];
+
+    oBtn.onclick = function () {
+        if(Members.includes[oUser.value]&&oPwd.value=="代码岛民主党万岁")
+        {
+            localStorage.setItem("user", oUser.value);
+        }
+        // if (oUser.value != "" && oPwd.value != ""&&oUser.value==localStorage.getItem("user")&&oPwd.value==localStorage.getItem("pwd")) {
+        //     localStorage.setItem("user", oUser.value);
+        //     localStorage.setItem("pwd", oPwd.value);
+        // }
+        else {
+            oText.innerHTML = "用户名或密码错误！";
+        }
+    }
+}
