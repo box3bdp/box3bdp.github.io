8e84030ffacfb101a483e2b1cc04058a6d709103
box3bdp <101050018+box3bdp@users.noreply.github.com>
2022-09-02 20:11:41 +0800

    Update index.js

--- sign-in/index.js
+++ sign-in/index.js
@@ -2,7 +2,7 @@ window.onload = function () {
     var oUser = document.getElementsByName("username");
     var oPwd = document.getElementsByName("password");
     var oBtn = document.getElementsByClassName("btn");
-    var oText = document.getElementByClassName("text");
+    var oText = document.getElementById("text");
 
     var Members=['build_一只建筑师吖'];
 
@@ -10,6 +10,7 @@ window.onload = function () {
         if(Members.includes[oUser.value]&&oPwd.value=="代码岛民主党万岁")
         {
             localStorage.setItem("user", oUser.value);
+            alert('s');
         }
         // if (oUser.value != "" && oPwd.value != ""&&oUser.value==localStorage.getItem("user")&&oPwd.value==localStorage.getItem("pwd")) {
         //     localStorage.setItem("user", oUser.value);
