d8b731de5447bdd7cffd6dc48a68cce0ae12e5fd
box3bdp <101050018+box3bdp@users.noreply.github.com>
2022-09-03 19:35:00 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
-window.onload=function()
-{
-    document.body.onselectstart=function()
-    {
+var btn = document.getElementById("btn");
+var pw = document.getElementById("pw");
+var sign = document.getElementById("sign");
+var main = document.getElementById("main")
+
+btn.onclick = function () {
+    if (pw.value == "bdp666") {
+        sign.style.display = "none";
+        main.style.display = "";
+    }
+}
+
+window.onload = function () {
+    document.body.onselectstart = function () {
         return false;
     }
 }
