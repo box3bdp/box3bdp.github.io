3ad1097bee79a6ead6b5d9718e55ba73dacb836e
box3bdp <101050018+box3bdp@users.noreply.github.com>
2022-09-04 12:05:15 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,21 @@
 var btn = document.getElementById("btn");
 var pw = document.getElementById("pw");
 var sign = document.getElementById("sign");
-var main = document.getElementById("main")
+var main = document.getElementById("main");
+var err = document.getElementById("error");
 
 btn.onclick = function () {
     if (pw.value == "bdp666") {
         sign.style.display = "none";
         main.style.display = "";
     }
+    else {
+        err.innerHTML = "密码错误！";
+    }
+}
+
+pw.onclick = function () {
+    err.innerHTML = "";
 }
 
 window.onload = function () {
