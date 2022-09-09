ea844e44a246780cda32c0021e6b01b8b87f7a69
box3bdp <101050018+box3bdp@users.noreply.github.com>
2022-09-09 20:45:09 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,7 +10,11 @@ btn.onclick = function () {
         main.style.display = "";
     }
     else {
-        err.innerHTML = "密码错误！";
+        err.innerHTML = `密码错误！<br/><a href="#" id="q">密码正确但无法登录？</a>`;
+        var q = document.getElementById("q");
+        q.onclick = function () {
+            alert("密码正确但无法登录的解决方法：\n因为党内有党员被开除党籍，按照相关规定，管理员需要更改密码。\n请时刻关注民主党各群的密码更新动态！")
+        }
     }
 }
 
@@ -21,5 +25,29 @@ pw.onclick = function () {
 window.onload = function () {
     document.body.onselectstart = function () {
         return false;
+    }
+}
+
+//禁用右键（防止右键查看源代码） 
+window.oncontextmenu = function () { return false; }
+//禁止任何键盘敲击事件（防止F12和shift+ctrl+i调起开发者工具） 
+window.onkeydown = window.onkeyup = window.onkeypress = function () {
+    window.event.returnValue = false;
+    return false;
+}
+//如果用户在工具栏调起开发者工具，那么判断浏览器的可视高度和可视宽度是否有改变，如有改变则关闭本页面 
+var h = window.innerHeight, w = window.innerWidth;
+window.onresize = function () {
+    if (h != window.innerHeight || w != window.innerWidth) {
+        window.close();
+        window.location = "https://www.bilibili.com/video/BV1hq4y1s7VH";
+    }
+}
+
+//F12
+window.onkeydown = window.onkeyup = window.onkeypress = function (event) {
+    if (event.keyCode == 123) {
+        event.preventDefault();
+        window.event.returnValue = false;
     }
 }
