558890b8aaf94c974a44ed58d6d70e27045dbf30
box3bdp <101050018+box3bdp@users.noreply.github.com>
2022-09-11 09:46:28 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -41,11 +41,12 @@ window.onload = function () {
 window.oncontextmenu = function () {
     return false;
 }
+
 //禁止任何键盘敲击事件（防止F12和shift+ctrl+i调起开发者工具） 
-window.onkeydown = window.onkeyup = window.onkeypress = function () {
-    window.event.returnValue = false;
-    return false;
-}
+// window.onkeydown = window.onkeyup = window.onkeypress = function () {
+//     window.event.returnValue = false;
+//     return false;
+// }
 
 //如果用户在工具栏调起开发者工具，那么判断浏览器的可视高度和可视宽度是否有改变，如有改变则关闭本页面 
 var h = window.innerHeight, w = window.innerWidth;
