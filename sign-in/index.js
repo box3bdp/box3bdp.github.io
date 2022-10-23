0e764f7103db28b8a7f2d5b2e2900530f0239c86
box3bdp <101050018+box3bdp@users.noreply.github.com>
2022-10-23 21:51:40 +0800

    Update index.js

--- sign-in/index.js
+++ sign-in/index.js
@@ -9,18 +9,19 @@ window.onload = function () {
     var members = [
         "build_%u4E00%u53EA%u5EFA%u7B51%u5E08%u5416",
         "%u4F60%u597D2050",
-        "%u4E0D%u6587%u660Eder%u5C9B%u6C11"
+        "%u4E0D%u6587%u660Eder%u5C9B%u6C11",
+        "BCM%u6708%u89C1%u8349"
     ];
 
     oBtn.onclick = function () {
         console.log();
-        if (members.includes(escape(oUser.value))) {
-            if (oPwd.value == rightpw) {
-                localStorage.setItem("user", oUser.value);
-                localStorage.setItem("pwd", oPwd.value);
-                localStorage.setItem("lspwd", oPwd.value);
-                localStorage.setItem("isin", "yes");
-                window.location = localStorage.getItem("yourpage");
+        if (members.includes(escape(oUser.value))) {    //如果用户名正确
+            if (oPwd.value == rightpw) {    //如果密码正确
+                localStorage.setItem("user", oUser.value);  //存储用户名
+                localStorage.setItem("pwd", oPwd.value);    //存储密码
+                localStorage.setItem("lspwd", oPwd.value);  //将本次登陆输入的密码当作lspwd的值，当密码变更时，会根据这个变量来提醒党员关注密码更新动态
+                localStorage.setItem("isin", "yes");    //将用户登陆状态标记为已登陆
+                window.location = localStorage.getItem("yourpage");     //跳转你一开始想访问的页面！
             }
             else {
                 if (localStorage.getItem("isin") == "yes" && oPwd.value != rightpw && oPwd.value == localStorage.getItem("lspwd")) {
@@ -37,8 +38,8 @@ window.onload = function () {
         }
     }
 
-    var code = '不文明der岛民';
-    var en = escape(code);
-    var de = unescape(en);
-    console.log(en, '    ', de)
+    // var code = 'BCM月见草';
+    // var en = escape(code);
+    // var de = unescape(en);
+    // console.log(en, '    ', de)
 }
